d638cd80fa2b303a42c1324b610964c8fde28c24
tpodsosony <talpods6@gmail.com>
2025-11-08 14:50:50 -0600

    Switched from .glob to collections for posts

---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob} from 'astro/loaders';
+
+// 3. Define your collection(s)
+const blogs = defineCollection({
+    loader: glob({ pattern: '**/*.mdx', base: './src/blog' }),
+//   schema: z.object({
+//     title: z.string(),
+//     pubDate: z.coerce.date(),
+//     description: z.string(),
+//     author: z.string()
+  });
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = {'blogs': blogs};
